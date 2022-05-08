dad7a10fa6901948ffaf51ea53d0c63d440ed130
sila0319 <102634882+sila0319@users.noreply.github.com>
2022-05-08 19:36:09 +0900

    [BOJ] 11047(동전 0)/S3/O

---
+++ Ryuwonkyu/BOJ/src/BOJ/BOJ_동전0_11047_S3.java
@@ -0,0 +1,36 @@
+package BOJ;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ_µ¿Àü0_11047_S3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int count =0;
+		int result =0;
+		Integer [] arr = new Integer[n];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr, Collections.reverseOrder());
+		while(true) {
+			count =0;
+			if(k==0) {
+				break;
+			}
+			for (int i = 0; i < arr.length; i++) {
+				if( k>=arr[i]) {
+					result += k/arr[i];
+					k %= arr[i];
+				}
+			}
+		System.out.println(result);
+		}
+	}
+
+}
