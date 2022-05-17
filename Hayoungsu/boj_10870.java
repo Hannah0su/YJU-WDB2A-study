b40fe6f19bd85815497fd6ebfa1e56052abf5ef7
Hannah0su <83005178+Hannah0su@users.noreply.github.com>
2022-05-17 11:15:56 +0900

    [BOJ] 10870(피보나치 수 5)/B2/O

--- Hayoungsu/boj_10870.java
+++ Hayoungsu/boj_10870.java
@@ -13,7 +13,7 @@ public class boj_10870 {
  
 	}
     
-	static int fibonacci(int n) {   //F(n) = F(n-1) + F(n-2)
+	static int fibonacci(int n) {   //F(n) = F(n-1) + F(n-2), 피보나치 수는 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597...
 		if (n == 0)//0과 1인 경우
             return 0;
 		else if (n == 1)
