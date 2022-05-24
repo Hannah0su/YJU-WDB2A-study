d56da2c8c80adbc6fb13b08ce2a992112d2ab319
sila0319 <102634882+sila0319@users.noreply.github.com>
2022-05-24 13:48:48 +0900

    [BOJ] 9012 (괄호) /S4 /O

---
+++ Ryuwonkyu/BOJ/src/BOJ/BOJ_괄호_9012_S4.java
@@ -0,0 +1,36 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_°ýÈ£_9012_S4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int count =0;
+		for (int i = 0; i < n; i++) {
+			String s = sc.next();
+			count =0;
+			if(s.length()%2==0) {
+				for (int j = 0; j < s.length(); j++) {
+					char c = s.charAt(j);
+					if(c=='(') {
+						count ++;
+					}else {
+						count--;
+						if(count==-1) {
+							break;
+						}
+					}
+				}
+				if(count==0) {
+				System.out.println("YES");
+				}else {
+					System.out.println("NO");
+				}
+			}else {
+				System.out.println("NO");
+			}
+		}
+		
+	}
+}
